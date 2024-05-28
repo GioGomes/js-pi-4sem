02ba18749262df47d9c0edd7647cbfbcd0273d35
GioGomes <132763382+GioGomes@users.noreply.github.com>
2024-05-28 16:46:28 -0300

    Polverizador-imagem

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,6 +7,7 @@ function About() {
                 <SimpleCard imgURL="src/assets/truck.jpg" title="Improve control" text="We are here to help" />
                 <SimpleCard imgURL="src/assets/truck.jpg" title="Improve control" text="We are here to help" />
                 <SimpleCard imgURL="src/assets/truck.jpg" title="Improve control" text="We are here to help" />
+                <SimpleCard imgURL="src/assets/pulverizador_m4040_foto_no_campo_large.jpg" title="Improve control" text="We are here to help"/>
         </Layout>
     )
 }
